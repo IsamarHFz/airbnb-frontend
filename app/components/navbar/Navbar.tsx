4f4ad56e126831abb783c1ce43e2b2813d68884e
IsabelHernandez <marisahfz07@gmail.com>
2025-02-27 22:25:00 -0600

    01 NavBar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+    return(
+        <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+            <div className="max-w-[1500px mx-auto px-6">
+                <div className="felx justify-between items-center">
+                    <Link href="/">
+                        <Image
+                            src="/logo.png"
+                            alt="DjangoBnb logo"
+                            width={180}
+                            height={38}
+                        />
+                    </Link>
+
+                    <div className="flex space-x-6">
+                        Saarch 
+                    </div>
+
+                    <div className="flex items-center space-x-6">
+                        Add property
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
