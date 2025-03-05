846d1136e167df6f9f4b18c39bbf6c09671c222e
IsabelHernandez <marisahfz07@gmail.com>
2025-03-05 15:46:04 -0600

    06 My properties and Inbox page (static)

---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,29 @@
+const ConversationDetail = () => {
+    return(
+        <>
+            <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+                <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+                    <p className="font-bold text-gray-500">John Doe</p>
+
+                    <p>Hola django y airbnb </p>
+                </div>
+
+                <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-airbnb">
+                    <p className="font-bold text-white">Code With Stein</p>
+
+                    <p className="text-white">Hola django y airbnb </p>
+                </div>
+            </div>
+
+            <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+                <input 
+                    type="text" 
+                    placeholder="Type your message..."
+                    className="w-full p-2 bg-gray-200 rounded-xl"
+                />
+            </div>
+        </>
+    )
+}
+
+export default ConversationDetail;
